ca9c60346e31ca19553f154042390e254f805154
Fox-Programs <alexis.facon@ynov.com>
2026-04-27 16:23:13 +0200

    Fixies base, entities, game_map and player

--- models/base.py
+++ models/base.py
@@ -1,3 +1,29 @@
+from enums import State
+from game import GameObject
+
+
+def destroy(game : GameObject = game) -> None:
+    game.end_game("Attacker")
+
+
 class BaseObject:
-    def __init__(self, _id):
+    def __init__(self,
+                 _id : int,
+                 _hp : int,
+                 _max_hp : int,
+                 _x : int,
+                 _y : int,
+                 ):
         self.id = _id
+        self.hp = _hp
+        self.max_hp = _max_hp
+        self.x = _x
+        self.y = _y
+
+    def get_attacked(self, damage) -> None:
+        self.hp -= damage
+        if self.hp <= 0:
+            destroy()
+
+
+
--- models/player.py
+++ models/player.py
@@ -2,16 +2,17 @@ from typing import List
 from exceptions import InsufficientFundsError
 from enums import Team, UnitType, TowerType, Placement
 from game_map import MapObject
-from entities import AttackObject, DefenseObject
+from entities import UnitObject, TowerObject
 from base import BaseObject
 
 
 class PlayerObject:
-    def __init__(self, _id: int, _username : str, _gold : int, _team : Team):
+    def __init__(self, _id: int, _username : str, _gold : int, _team : Team, _map : MapObject ) -> None:
         self.id = _id
         self.username = _username
         self.gold = _gold
         self.team = _team
+        self.map = _map
 
     def can_afford(self, item_amount) -> bool:
         return item_amount <= self.gold
@@ -34,17 +35,17 @@ class PlayerObject:
         return obj
 
 class AttackerPlayer(PlayerObject):
-    def __init__(self, _id, _username, _gold, _team,  _units_sent : List[AttackObject], _income_rate : int):
-        super().__init__(_id, _username, _gold, _team)
+    def __init__(self, _id, _username, _gold, _team, _map,  _units_sent : List[UnitObject], _income_rate : int):
+        super().__init__(_id, _username, _gold, _team, _map)
         self.units_sent = _units_sent
         self.income_rate = _income_rate
 
-    def send_unit(self, unit_type : UnitType, _map : MapObject ) -> None:
+    def send_unit(self, unit_type : UnitType) -> None:
         if not self.can_afford(unit_type.value) :
             return
 
-        new_attack_obj = AttackObject(1, unit_type, 1, 1, 1, 1, 1.0, 1, 1, 1, 1, 1, True, (1,1))
-        _map.spawn_unit(new_attack_obj)
+        new_attack_obj = UnitObject(1, unit_type, 1, 1, 1, 1, 1.0, 1, 1, 1, 1,self.map, 1, True, (1,1))
+        self.map.spawn_unit(new_attack_obj)
 
         self.spend(unit_type.value)
 
@@ -53,28 +54,28 @@ class AttackerPlayer(PlayerObject):
 
 
 class DefenderPlayer(PlayerObject):
-    def __init__(self, _id, _username, _gold, _team,  _towers : List[DefenseObject], _base : BaseObject):
+    def __init__(self, _id, _username, _gold, _team,  _towers : List[TowerObject], _base : BaseObject):
         super().__init__(_id, _username, _gold, _team)
         self.towers = _towers
         self.base = _base
 
-    def place_tower(self, tower : DefenseObject, x : int, y : int, _map : MapObject) -> None:
+    def place_tower(self, tower : TowerObject, x : int, y : int, _map : MapObject) -> None:
         if not self.can_afford(tower.price) :
             return
 
 
         # Valeur de l'objet à changer
-        new_def_obj = DefenseObject(1, TowerType.ARCHER, 1, 1, 1, 1, 1.0, 1, 1, x, y, None, 1, Placement.EDGE, 1.0)
+        new_def_obj = TowerObject(1, TowerType.ARCHER, 1, 1, 1, 1, 1.0, 1, 1, x, y, None, 1, Placement.EDGE, 1.0)
 
 
         new_def_obj.deploy(_map)
         self.spend(tower.price)
 
-    def sell_tower(self, tower : DefenseObject, _map : MapObject) -> None:
+    def sell_tower(self, tower : TowerObject,) -> None:
         self.earn(round(tower.price * 0.7))
-        _map.remove_tower(tower)
+        self.map.remove_tower(tower)
 
-    def upgrade_tower(self, tower : DefenseObject) -> None:
+    def upgrade_tower(self, tower : TowerObject) -> None:
         if not self.can_afford(tower.price):
             return
         self.spend(tower.price)
